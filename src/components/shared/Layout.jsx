3f6e1df9d850d17bec4e7541fb583c586081acca
Alejandro Martí <alexymartiygisbert@gmail.com>
2023-07-01 17:13:23 +0200

    Terminado menu lateral
    Terminada la implementación del menú lateral

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
 import Sidebar from './Sidebar'
+import Header from './Header'
 
 export default function Layout()
 {
     return (
         <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hiden'>
            <Sidebar />
-           <div className="p-4">
+           <div className="flex-l">
+            <Header/>
            <div className='bg-teal-200'>header</div>
                 <div>{<Outlet/>}</div>
            </div>
--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import {FcMenu} from 'react-icons/fc'
-import {Link} from 'react-router-dom'
+import {Link,useLocation} from 'react-router-dom'
 import {SIDEBAR_LINKS} from '../../lib/consts/navigation'
+import classNames from 'classnames';
 
 const linkClasses2 = 'flex items-center gap-2 font-light px-3 py-3 hover:bg-neutral-700 hover:no-underline cative:bg-neutral-600 rounded-sm text-base';
-const linkClasses = 'flex justify-center pt-3 pb-3 ';
-const linkClasses3='gap-2 font-light hover:bg-neutral-700 hover:no-underline cative:bg-neutral-600 rounded-sm text-base';
+const linkClasses3 = 'flex justify-center pt-3 pb-3 ';
+const linkClasses='gap-2 font-light hover:bg-neutral-700 hover:no-underline cative:bg-neutral-600 rounded-sm text-base';
 export default function Sidebar()
 {
     return (
         <div className='sidebar w-20 pt-3 flex flex-col text-white'>
-            <div className='flex justify-center pt-3 pb-10 '>
+            <div className='flex justify-center pt-3 pb-10 border-b border-neutral-400'>
                 <FcMenu fontSize={30}/>
                 <span className='text-neutral -100 text-lg'></span>
             </div>
@@ -25,11 +26,13 @@ export default function Sidebar()
     )
 }
 function SidebarLink({item}){
+    const {pathname}=useLocation();
+
     return(
         
-        <Link to={item.path} className={linkClasses3}>
+        <Link to={item.path} className={classNames(pathname === item.path ? 'bg-sky-700' : 'bg-inherit', linkClasses)}>
            
-            <div className=' flex justify-center pt-5 pb-5 hover:bg-sky-700'>
+            <div className='bg-inherit flex justify-center pt-5 pb-5 hover:bg-slate-500'>
                 <span className='text-neutral -100 text-2xl'>{item.icon}</span>
             </div>
         </Link>
